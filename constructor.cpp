00a8fba482a52fc921d5144f1421a684a13a109b
vivekdev <techievivek2023@gmail.com>
2023-12-13 11:09:05 +0530

    13-12-2023

--- constructor.cpp
+++ constructor.cpp
@@ -5,3 +5,35 @@
                 type -> defuclt and parameterized , copy 
 */
 
+#include<iostream>
+using namespace std;
+class Test{
+    int z;
+    public:
+    Test()  //Defualt constructor
+    {
+        cout<<"Defualt constructor is called."<<endl;
+    }
+
+    Test(int x )    // parameterized constructor 
+    {
+        z=x;
+        cout<<"parameterized constructor is called."<<endl
+        <<"value of x is: "<<z <<endl;
+    }
+
+    Test(Test &x)          // copyconstructor 
+    {
+         int m= x.z;
+        cout<<"parameterized constructor is called."<<endl
+        <<"value of x is: "<<m <<endl;
+    }
+
+    void hello() {
+        cout<<"Hello Wolrd....";
+    }
+};
+
+int main(){
+    Test a(10), t(a);
+}
