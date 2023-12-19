333ba9338183eefcdd275ede6fb759ccf3a4f945
vivekdev <techievivek2023@gmail.com>
2023-12-19 09:43:27 +0530

    19-12-2023

---
+++ Oops/hirechical.cpp
@@ -0,0 +1,51 @@
+// hirechical inheritance
+#include<iostream>
+using namespace std;
+class Base
+{
+    protected:
+    int a;
+     public:
+    void SetA(){
+        cout << "Enter a: ";
+        cin>>a;
+    }
+};
+
+class Derived1 :public Base
+{
+    int b;
+    public:
+    void setB(){
+        cout << "Enter b: ";
+        cin>>b;
+    }
+    void getData(){
+        cout<<"Product of a and b is: "<<a*b<<endl;
+    }
+};
+class Derived2 :public Base
+{
+    int c;
+    public:
+    void setC(){
+        cout << "Enter c: ";
+        cin>>c;
+    }
+    void getdata(){
+        cout<<"Sum of a and c is: "<<a+c<<endl;
+    }
+};
+
+int main()
+{
+    Derived1 d1;
+    d1.SetA();
+    d1.setB();
+    d1.getData();
+
+    Derived2 d2;
+    d2.SetA();
+    d2.setC();
+    d2.getdata();
+}
---
+++ Oops/hybrid.cpp
@@ -0,0 +1,51 @@
+// hybrid (virtual) inheritance
+#include<iostream>
+using namespace std;
+class Base
+{
+    protected:
+    int a;
+     public:
+    void SetA(){
+        cout << "Enter a: ";
+        cin>>a;
+    }
+};
+
+class Derived1 :public Base
+{
+    protected:
+    int b;
+    public:
+    void setB(){
+        cout << "Enter b: ";
+        cin>>b;
+    }
+};
+class Derived2 
+{
+    protected:
+    int c;
+    public:
+    void setC(){
+        cout << "Enter c: ";
+        cin>>c;
+    }
+};
+class ABC :public Derived1, public Derived2
+{
+    public:
+    void getData(){
+        cout<<"Product of a, b and c is: "<<a*b*c<<endl;
+    }
+};
+
+
+int main()
+{
+    ABC t;
+    t.SetA();
+    t.setB();
+    t.setC();
+    t.getData();
+}
